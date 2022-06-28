5a71c4b8da1c624bcf34d1c6ffc45cb7c1e4b06a
Graphicanimal <newhookliam@gmail.com>
2022-06-28 17:45:03 +0100

    was removed from folder for trial on Newhookliam.github.io

---
+++ todoList.js
@@ -0,0 +1,43 @@
+const input = document.querySelector('#inputItem');
+const ulImportant = document.querySelector('#important');
+const radImportant = document.querySelector('#radioImportant');
+const btn = document.querySelector('#btn');
+const radNice = document.querySelector('#radioNice');
+const ulNice = document.querySelector('#nice');
+const combinedUl = document.querySelectorAll('.UL');
+
+btn.addEventListener('click', function (e) {
+    if (radImportant.checked) {
+        e.preventDefault();
+        const item = input.value;
+        const newLi = document.createElement('LI');
+        newLi.innerText = item;
+        ulImportant.append(newLi);
+        ulImportant.style.listStyle = 'none';
+
+        input.value = "";
+        e.stopImmediatePropagation();
+    }
+});
+
+btn.addEventListener('click', function (e) {
+    if (radNice.checked)
+        e.preventDefault();
+    const item2 = input.value;
+    const newLi2 = document.createElement('LI');
+    newLi2.innerText = item2;
+    ulNice.append(newLi2);
+    ulNice.style.listStyle = 'none';
+
+    input.value = "";
+})
+
+ulNice.addEventListener('click', function (e) {
+    e.target.nodeName === "LI" && e.target.remove();
+})
+ulImportant.addEventListener('click', function (e) {
+    e.target.nodeName === "LI" && e.target.remove();
+})
+
+
+
