d39081a00ffcb2b8c61712eb967d4cecbeeae4fc
Graphicanimal <newhookliam@gmail.com>
2022-06-30 17:17:51 +0100

    changes made to the card colors on hover

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const content = document.querySelector('.dropdown-content');
 const dropdown = document.querySelector('.dropdown');
 const items = document.querySelector('.dropdown', 'dropdown-content');
 
-
-
 btn.addEventListener('mouseover', function () {
     content.classList.toggle('show');
 })
